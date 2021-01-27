335c9b459cfaf12b8c1b802b12763fa90a1a3a81
Samuel Koskelainen <samuelkoskelainen@gmail.com>
2021-01-27 11:23:01 +0200

    Axios and loader components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,17 +1,21 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, Fragment, SyntheticEvent } from 'react';
 import { Container } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
-import axios from 'axios';
 import {IActivity} from '../models/activity'
 import NavBar from '../../features/nav/NavBar'
 import ActivityDashBoard from '../../features/activities/dashboard/ActivityDashBoard';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 
 
 const App:React.FC = () => {
-  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [activities, setActivities] = useState<IActivity[]>([])
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
-  const [editMode, setEditMode] = useState(false);
+  const [editMode, setEditMode] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [submitting, setSubmitting] = useState(false)
+  const [target, setTarget] = useState("")
 
   const handleSelectedActivity = (id: string) => {
     setSelectedActivity(activities.filter(a => a.id === id)[0])
@@ -23,33 +27,45 @@ const App:React.FC = () => {
   }
 
   const handleCreateActivity = (activity: IActivity) => {
-    setActivities([...activities, activity])
-    setSelectedActivity(activity);
-    setEditMode(false)
+    setSubmitting(true)
+    agent.Activities.create(activity).then(() => {
+      setActivities([...activities, activity])
+      setSelectedActivity(activity);
+      setEditMode(false)
+    }).then(() => setSubmitting(false))
   }
 
   const handleEditActivity = (activity: IActivity) => {
-    setActivities([...activities.filter(a => a.id !== activity.id), activity])
-    setSelectedActivity(activity)
-    setEditMode(false)
+    setSubmitting(true)
+    agent.Activities.update(activity).then(() => {
+      setActivities([...activities.filter(a => a.id !== activity.id), activity])
+      setSelectedActivity(activity)
+      setEditMode(false)
+    }).then(() => setSubmitting(false))
   }
   
-  const handleDeleteActivity = (id: string) => {
-    setActivities([...activities.filter(a => a.id !== id)])
+  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
+    setSubmitting(true)
+    setTarget(event.currentTarget.name)
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter(a => a.id !== id)])
+    }).then(() => setSubmitting(false))
   }
 
   useEffect(() => {
-    axios
-      .get<IActivity[]>("/api/activities")
+    agent.Activities
+      .list()
       .then((response) => {
         let activities: IActivity[] = [];
-        response.data.forEach(activity => {
+        response.forEach((activity) => {
           activity.date = activity.date.split(".")[0]
           activities.push(activity)
         })
         setActivities(activities)
-      })
+      }).then(() => setLoading(false))
   }, [])
+
+  if (loading) return <LoadingComponent content="Loading activities..." />
 
   return (
     <Fragment>
@@ -65,6 +81,8 @@ const App:React.FC = () => {
         createActivity={handleCreateActivity}
         editActivity={handleEditActivity}
         deleteActivity={handleDeleteActivity}
+        submitting={submitting}
+        target={target}
         />
       </Container>
     </Fragment>
--- client-app/src/features/form/ActivityForm.tsx
+++ client-app/src/features/form/ActivityForm.tsx
@@ -8,13 +8,15 @@ interface IProps {
   activity: IActivity;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
+  submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
   setEditMode, 
   activity: initialFormState,
   createActivity,
-  editActivity
+  editActivity,
+  submitting
 }) => {
   
   const initializeForm = () => {
@@ -95,6 +97,7 @@ const ActivityForm: React.FC<IProps> = ({
         value={activity.venue} 
         />
         <Button 
+        loading={submitting}
         floated="right" 
         positive 
         type="submit" 
